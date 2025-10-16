acbcbb547e544326fd5ec27af6b801d8755954a3
omar-maarouf <omar.maarouf@ejada.com>
2025-10-16 16:31:43 +0300

    created the four main controllers and their relative views

---
+++ SurveyMVC/Controllers/ResponsesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SurveyMVC.Models;
+
+namespace SurveyMVC.Controllers
+{
+    public class ResponsesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Responses
+        public ActionResult Index()
+        {
+            var responses = db.Responses.Include(r => r.Survey);
+            return View(responses.ToList());
+        }
+
+        // GET: Responses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // GET: Responses/Create
+        public ActionResult Create()
+        {
+            ViewBag.SurveyId = new SelectList(db.Surveys, "Id", "AdminId");
+            return View();
+        }
+
+        // POST: Responses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SurveyId,EmployeeId")] Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Responses.Add(response);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SurveyId = new SelectList(db.Surveys, "Id", "AdminId", response.SurveyId);
+            return View(response);
+        }
+
+        // GET: Responses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SurveyId = new SelectList(db.Surveys, "Id", "AdminId", response.SurveyId);
+            return View(response);
+        }
+
+        // POST: Responses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SurveyId,EmployeeId")] Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(response).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SurveyId = new SelectList(db.Surveys, "Id", "AdminId", response.SurveyId);
+            return View(response);
+        }
+
+        // GET: Responses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // POST: Responses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Response response = db.Responses.Find(id);
+            db.Responses.Remove(response);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
