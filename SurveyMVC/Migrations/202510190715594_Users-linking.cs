509c2561485f382a40b106f3882918cfab2f9223
omar-maarouf <omar.maarouf@ejada.com>
2025-10-19 10:19:46 +0300

    linking users with surveys and responses tables

---
+++ SurveyMVC/Migrations/202510190715594_Users-linking.cs
@@ -0,0 +1,28 @@
+﻿namespace SurveyMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Userslinking : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Surveys", "AdminId", c => c.String(maxLength: 128));
+            AlterColumn("dbo.Responses", "EmployeeId", c => c.String(maxLength: 128));
+            CreateIndex("dbo.Surveys", "AdminId");
+            CreateIndex("dbo.Responses", "EmployeeId");
+            AddForeignKey("dbo.Surveys", "AdminId", "dbo.AspNetUsers", "Id");
+            AddForeignKey("dbo.Responses", "EmployeeId", "dbo.AspNetUsers", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Responses", "EmployeeId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Surveys", "AdminId", "dbo.AspNetUsers");
+            DropIndex("dbo.Responses", new[] { "EmployeeId" });
+            DropIndex("dbo.Surveys", new[] { "AdminId" });
+            AlterColumn("dbo.Responses", "EmployeeId", c => c.String());
+            AlterColumn("dbo.Surveys", "AdminId", c => c.String());
+        }
+    }
+}
